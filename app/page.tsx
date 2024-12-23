b6af492bbd4bf1303e32e39b441e71eb8c58903a
aditya-2k23 <holaaditya123@gmail.com>
2024-12-24 00:14:22 +0530

    Update Home component layout for improved centering and font styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="center">
-      <h1 className="text-3xl text-brand">
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl font-bold">
         StoreIt - The only solution you need
       </h1>
     </div>
