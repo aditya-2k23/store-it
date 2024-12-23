590a09316f49c44ab04d2c810fa1836e4a0a79f0
aditya-2k23 <holaaditya123@gmail.com>
2024-12-24 00:12:20 +0530

    Refactor global styles to enhance typography, scrollbar customization, and layout utilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center bg-amber-100">
+    <div className="center">
       <h1 className="text-3xl text-brand">
         StoreIt - The only solution you need
       </h1>
