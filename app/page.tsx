94499ca96cc03dccb3d354bd46bc74b2045e05d8
aditya-2k23 <holaaditya123@gmail.com>
2024-12-24 00:11:36 +0530

    Update Home component layout and styling for improved presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">StoreIt - The only solution you need</h1>
+    <div className="flex h-screen items-center justify-center bg-amber-100">
+      <h1 className="text-3xl text-brand">
+        StoreIt - The only solution you need
+      </h1>
     </div>
   );
 }
