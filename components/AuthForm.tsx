19525cd9698d406f1a219f853dd49bc05c437e8a
aditya-2k23 <holaaditya123@gmail.com>
2024-12-25 18:46:21 +0530

    Update AuthForm to conditionally render OTPModal based on accountId

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -155,7 +155,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
 
-      {true && (
+      {accountId && (
         <OTPModal email={form.getValues("email")} accountId={accountId} />
       )}
     </>
