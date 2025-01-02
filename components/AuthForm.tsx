13a8a7e814ddfb8f389fa5ff9eceb9c9bbc32490
aditya-2k23 <holaaditya123@gmail.com>
2025-01-02 16:37:40 +0530

    Refactor AuthForm component to use dynamic form schema based on form type

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,17 +18,23 @@ import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
 
-const formSchema = z.object({
-  fullName: z.string().min(2).max(50),
-  email: z.string().email(),
-});
-
 type FormType = "sign-in" | "sign-up";
+
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
