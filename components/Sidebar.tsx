9c5242424ebb9383515b3086efbfbf89a4b9def2
aditya-2k23 <holaaditya123@gmail.com>
2025-01-02 17:29:33 +0530

    Add navigation items to Sidebar component and create constants for nav items

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="sidebar">
       <Link href="/">
@@ -20,11 +27,34 @@ const Sidebar = () => {
           height={52}
           className="lg:hidden"
         />
-
-        <nav className="sidebar-nav">
-          <ul className="flex flex-1 flex-col gap-6"></ul>
-        </nav>
       </Link>
+
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active"
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active"
+                  )}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
     </aside>
   );
 };
