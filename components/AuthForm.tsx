7342d4403751b0095be18f782c637b412223e27b
aditya-2k23 <holaaditya123@gmail.com>
2025-01-02 16:25:30 +0530

    Refactor AuthForm component to simplify form schema and remove unused state variables
    This is a refactor regarding the app not working when signing up for a new user. So, I decided to reimplement this authForm :)

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "./ui/button";
 import {
   Form,
   FormControl,
@@ -14,30 +14,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { createAccount, signInUser } from "@/lib/actions/user.actions";
-import OTPModal from "./OTPModal";
+
+const formSchema = z.object({
+  fullName: z.string().min(2).max(50),
+  email: z.string().email(),
+});
 
 type FormType = "sign-in" | "sign-up";
 
-const authFormSchema = (formType: FormType) => {
-  return z.object({
-    email: z.string().email(),
-    fullName:
-      formType === "sign-up"
-        ? z.string().min(2).max(50)
-        : z.string().optional(),
-  });
-};
-
 const AuthForm = ({ type }: { type: FormType }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [accountId, setAccountId] = useState(null);
-
-  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -47,22 +32,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    setIsLoading(true);
-    setErrorMessage("");
-    try {
-      const user =
-        type === "sign-up"
-          ? await createAccount({
-              fullName: values.fullName || "",
-              email: values.email,
-            })
-          : await signInUser({ email: values.email });
-      setAccountId(user.accountId);
-    } catch {
-      setErrorMessage("Failed to create account. Please try again.");
-    } finally {
-      setIsLoading(false);
-    }
+    console.log(values);
   };
 
   return (
@@ -83,8 +53,8 @@ const AuthForm = ({ type }: { type: FormType }) => {
 
                     <FormControl>
                       <Input
-                        placeholder="Enter your full name"
                         className="shad-input"
+                        placeholder="Enter your full name"
                         {...field}
                       />
                     </FormControl>
@@ -106,8 +76,8 @@ const AuthForm = ({ type }: { type: FormType }) => {
 
                   <FormControl>
                     <Input
-                      placeholder="Enter your email"
                       className="shad-input"
+                      placeholder="Enter your email"
                       {...field}
                     />
                   </FormControl>
@@ -117,49 +87,10 @@ const AuthForm = ({ type }: { type: FormType }) => {
               </FormItem>
             )}
           />
-
-          <Button
-            type="submit"
-            className="form-submit-button"
-            disabled={isLoading}
-          >
-            {type === "sign-in" ? "Sign In" : "Sign Up"}
-
-            {isLoading && (
-              <Image
-                src="/assets/icons/loader.svg"
-                alt="loader"
-                width={24}
-                height={24}
-                className="ml-2 animate-spin"
-              />
-            )}
-          </Button>
-
-          {errorMessage && <p className="error-message">*{errorMessage}</p>}
-
-          <div className="body-2 flex justify-center">
-            <p className="text-light-100">
-              {type === "sign-in"
-                ? "Don't have an account?"
-                : "Already have an account?"}
-            </p>
-            <Link
-              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
-              className="ml-1 font-medium text-brand"
-            >
-              {" "}
-              {type === "sign-in" ? "Sign Up" : "Sign In"}
-            </Link>
-          </div>
+          <Button type="submit">Submit</Button>
         </form>
       </Form>
-
-      {accountId && (
-        <OTPModal email={form.getValues("email")} accountId={accountId} />
-      )}
     </>
   );
 };
-
 export default AuthForm;
