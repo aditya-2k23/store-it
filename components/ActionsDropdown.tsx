dab262852e962f286c5a86e991594e489b801d3f
aditya-2k23 <holaaditya123@gmail.com>
2025-01-03 19:31:13 +0530

    Add rename functionality to ActionsDropdown for file management

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -23,6 +23,8 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { renameFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 const ActionsDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -30,6 +32,8 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
   const [action, setAction] = useState<ActionType | null>(null);
   const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+
+  const path = usePathname();
 
   const closeAllModals = () => {
     setIsModalOpen(false);
@@ -39,7 +43,30 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
     // setEmails([]);
   };
 
-  const handleAction = async (action: ActionType) => {};
+  const handleAction = async (action: ActionType) => {
+    if (!action) return;
+    setIsLoading(true);
+
+    let success = false;
+
+    const actions = {
+      rename: () =>
+        renameFile({
+          fileId: file.$id,
+          name,
+          extension: file.extension,
+          path,
+        }),
+      share: () => console.log("share"),
+      delete: () => console.log("delete"),
+    };
+
+    success = await actions[action.value as keyof typeof actions]();
+
+    if (success) closeAllModals();
+
+    setIsLoading(false);
+  };
 
   const renderDialogContent = () => {
     if (!action) return null;
