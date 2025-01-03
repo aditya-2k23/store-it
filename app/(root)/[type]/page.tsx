5994b9909c3f9451dbab06c24b5403f7d1ab1643
aditya-2k23 <holaaditya123@gmail.com>
2025-01-03 18:40:36 +0530

    Add total size section to Page component for displaying data size

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -7,6 +7,16 @@ const Page = async ({ params }: SearchParamProps) => {
     <div className="page-container">
       <section className="w-full">
         <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0 MB</span>
+          </p>
+
+          <div className="sort-container">
+            <p className="body-1 hidden sm:block text-light-200">Sort by:</p>
+          </div>
+        </div>
       </section>
     </div>
   );
