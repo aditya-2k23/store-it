9a831fab84cb4b05effd2eb41c5b0315f222fd67
aditya-2k23 <holaaditya123@gmail.com>
2025-01-03 19:16:27 +0530

    Implement ActionsDropdown component with modal and dropdown functionality for file actions

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -1,8 +1,79 @@
+"use client";
+
 import { Models } from "node-appwrite";
-import React from "react";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Dialog } from "./ui/dialog";
+import Image from "next/image";
+import { actionsDropdownItems } from "@/constants";
+import Link from "next/link";
+import { constructDownloadUrl } from "@/lib/utils";
 
 const ActionsDropdown = ({ file }: { file: Models.Document }) => {
-  return <div>ActionsDropdown</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [action, setAction] = useState<ActionType | null>(null);
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image
+            src="/assets/icons/dots.svg"
+            alt="dots"
+            width={34}
+            height={34}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="max-w-[200px] truncate">
+            {file.name}
+          </DropdownMenuLabel>
+
+          <DropdownMenuSeparator />
+
+          {actionsDropdownItems.map((actionItem) => (
+            <DropdownMenuItem
+              key={actionItem.value}
+              className="shad-dropdown-item"
+              onClick={() => {
+                setAction(actionItem);
+
+                if (
+                  ["rename", "share", "delete", "details"].includes(
+                    actionItem.value
+                  )
+                ) {
+                  setIsModalOpen(true);
+                }
+              }}
+            >
+              <Link
+                href={constructDownloadUrl(file.bucketFileId)}
+                download={file.name}
+                className="flex items-center gap-2"
+              >
+                <Image
+                  src={actionItem.icon}
+                  alt={actionItem.label}
+                  width={30}
+                  height={30}
+                />
+                {actionItem.label}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  );
 };
 
 export default ActionsDropdown;
