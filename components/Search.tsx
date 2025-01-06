68c82daeb9db375615f2692719dd203633daf092
aditya-2k23 <holaaditya123@gmail.com>
2025-01-06 23:42:57 +0530

    Implement search functionality with dynamic results and query handling

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,37 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "./ui/input";
+import { useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
 
 const Search = () => {
   const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({ searchText: query });
+
+      setResults(files.documents);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
 
   return (
     <div className="search">
@@ -23,6 +49,40 @@ const Search = () => {
           className="search-input"
           onChange={(e) => setQuery(e.target.value)}
         />
+
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  key={file.$id}
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">
+                No files found for <span className="font-bold">{query}</span>
+              </p>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
